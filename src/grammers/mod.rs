99150113fed9599b45851b6ddc31a4ed9cf7f79f
Aza Maulana <azamaulanaaa@gmail.com>
2025-12-15 10:21:59 +0000

    feat(grammers): add authe and authz

--- src/grammers/mod.rs
+++ src/grammers/mod.rs
@@ -1,6 +1,6 @@
 use std::{fmt::Display, path::Path, sync::Arc};
 
-use grammers_client::Client;
+use grammers_client::{Client, types::User};
 use grammers_mtsender::SenderPool;
 use grammers_session::storages::SqliteSession;
 
@@ -8,6 +8,10 @@ use grammers_session::storages::SqliteSession;
 pub enum GrammersErrorKind {
     #[error("Session file: {0}")]
     SessionFile(&'static str),
+    #[error("Authentication: {0}")]
+    Authentication(&'static str),
+    #[error("Other: {0}")]
+    Other(&'static str),
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -27,10 +31,11 @@ pub struct Grammers {
     session: Arc<SqliteSession>,
     pool: SenderPool,
     client: Client,
+    user: Option<User>,
 }
 
 impl Grammers {
-    pub fn new(api_id: i32, session_file: &Path) -> Result<Self, GrammersError> {
+    fn new(api_id: i32, session_file: &Path) -> Result<Self, GrammersError> {
         let session = {
             let session = SqliteSession::open(session_file).map_err(|e| GrammersError {
                 kind: GrammersErrorKind::SessionFile("Something wrong with it"),
@@ -48,6 +53,52 @@ impl Grammers {
             session,
             pool,
             client,
+            user: None,
         })
+    }
+
+    pub async fn init(api_id: i32, session_file: &Path) -> Result<Self, GrammersError> {
+        let mut grammers = Self::new(api_id, session_file)?;
+
+        let is_authorized = grammers
+            .client
+            .is_authorized()
+            .await
+            .map_err(|e| GrammersError {
+                kind: GrammersErrorKind::Other("Unable to check authorization"),
+                source: Some(Box::new(e)),
+            })?;
+        if is_authorized {
+            let user = grammers.client.get_me().await.map_err(|e| GrammersError {
+                kind: GrammersErrorKind::Other("Unable to get user as me"),
+                source: Some(Box::new(e)),
+            })?;
+            grammers.user = Some(user);
+        }
+
+        Ok(grammers)
+    }
+
+    pub fn is_authorized(&self) -> bool {
+        self.user.is_some()
+    }
+
+    pub async fn authenticate(
+        &mut self,
+        bot_token: &str,
+        bot_api_hash: &str,
+    ) -> Result<(), GrammersError> {
+        let user = self
+            .client
+            .bot_sign_in(bot_token, bot_api_hash)
+            .await
+            .map_err(|e| GrammersError {
+                kind: GrammersErrorKind::Authentication("Something wrong with it"),
+                source: Some(Box::new(e)),
+            })?;
+
+        self.user = Some(user);
+
+        Ok(())
     }
 }
