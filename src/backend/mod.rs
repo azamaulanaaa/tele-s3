52e6b54e2bb1e1b477fa0bd1b5865afef7bbe49c
Aza Maulana <azamaulanaaa@gmail.com>
2025-12-19 09:31:13 +0000

    feat(backend): change design to reader approach only

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -3,16 +3,15 @@ use futures::io::{AsyncRead, AsyncWrite};
 use std::pin::Pin;
 
 pub type BoxedAsyncReader = Pin<Box<dyn AsyncRead + Send + Sync + Unpin>>;
-pub type BoxedAsyncWriter = Pin<Box<dyn AsyncWrite + Send + Sync + Unpin>>;
 
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum BackendError {}
 
 #[async_trait]
 pub trait Backend: Send + Sync + 'static {
-    async fn get_writer(&self, key: &str, size: u64) -> Result<BoxedAsyncWriter, BackendError>;
+    async fn write(&self, size: u64, reader: BoxedAsyncReader) -> Result<String, BackendError>;
 
-    async fn get_reader(&self, key: &str) -> Result<Option<BoxedAsyncReader>, BackendError>;
+    async fn read(&self, key: &str) -> Result<Option<BoxedAsyncReader>, BackendError>;
 
     async fn delete(&self, key: &str) -> Result<(), BackendError>;
 }
