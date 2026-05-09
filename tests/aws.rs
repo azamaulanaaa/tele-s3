0511b5381253888c3c752fcae31e6656789d6d46
Aza Maulana <azamaulanaaa@gmail.com>
2026-05-09 21:58:08 +0700

    test(aws): add delete bucket

--- tests/aws.rs
+++ tests/aws.rs
@@ -53,3 +53,46 @@ async fn test_create_bucket_and_list_bucket() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_delete_bucket() -> anyhow::Result<()> {
+    let config = config::<0, 0>().await?;
+    let client = Client::new(&config);
+
+    let bucket_name = "create-and-list";
+
+    {
+        let location = BucketLocationConstraint::from(REGION);
+        let cfg = CreateBucketConfiguration::builder()
+            .location_constraint(location)
+            .build();
+
+        let _ = client
+            .create_bucket()
+            .create_bucket_configuration(cfg)
+            .bucket(bucket_name)
+            .send()
+            .await
+            .context("create bucket")?;
+    }
+
+    let _ = client
+        .delete_bucket()
+        .bucket(bucket_name)
+        .send()
+        .await
+        .context("delete bucket")?;
+
+    let is_exists = {
+        let res = client.list_buckets().send().await.context("list bucket")?;
+        let is_exists = res
+            .buckets()
+            .iter()
+            .any(|bucket| bucket.name().is_some_and(|name| name == bucket_name));
+
+        is_exists
+    };
+    assert!(!is_exists, "buckets should not exist after delete");
+
+    Ok(())
+}
