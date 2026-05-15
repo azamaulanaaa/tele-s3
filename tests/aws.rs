cb3e74f93ffa51eba28339167316c28fcd1b84c4
Aza Maulana <azamaulanaaa@gmail.com>
2026-05-15 16:39:29 +0700

    test(aws): add list objects v2 with continuation token

--- tests/aws.rs
+++ tests/aws.rs
@@ -685,3 +685,79 @@ async fn test_list_objects_with_next_marker() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_list_objects_v2_with_continuation_token() -> anyhow::Result<()> {
+    let config = config::<3, 1024>().await?;
+    let client = Client::new(&config);
+
+    let bucket_name = "list-objects-v2-with-continuation-token";
+
+    {
+        let location = BucketLocationConstraint::from(REGION);
+        let cfg = CreateBucketConfiguration::builder()
+            .location_constraint(location)
+            .build();
+
+        let _ = client
+            .create_bucket()
+            .create_bucket_configuration(cfg)
+            .bucket(bucket_name)
+            .send()
+            .await
+            .context("create bucket")?;
+    }
+
+    let objects_data = [
+        ["prefix/item", "test_content"],
+        ["prefix/sub_item/item", "test_content"],
+        ["prefix_item", "test_content"],
+    ];
+
+    for &[key, content] in objects_data.iter() {
+        let _ = client
+            .put_object()
+            .bucket(bucket_name)
+            .key(key)
+            .body(ByteStream::from_static(content.as_bytes()))
+            .send()
+            .await
+            .context("put object")?;
+    }
+
+    let (objects_list, continuation_token) = {
+        let res = client
+            .list_objects_v2()
+            .bucket(bucket_name)
+            .max_keys(1)
+            .send()
+            .await
+            .context("list objects")?;
+        (
+            res.contents().to_owned(),
+            res.next_continuation_token().map(|v| v.to_owned()),
+        )
+    };
+
+    assert_eq!(objects_list.len(), 1, "length of object list is not 1");
+    assert_eq!(objects_list[0].key(), Some(objects_data[0][0]));
+
+    let continuation_token = continuation_token.expect("continuation token is missing");
+
+    let objects_list = {
+        let res = client
+            .list_objects_v2()
+            .bucket(bucket_name)
+            .max_keys(1)
+            .continuation_token(continuation_token)
+            .send()
+            .await
+            .context("list objects with continuation token")?;
+        res.contents().to_owned()
+    };
+
+    assert_eq!(objects_list.len(), 1, "length of object list is not 1");
+    assert_eq!(objects_list[0].key(), Some(objects_data[1][0]));
+
+    Ok(())
+}
